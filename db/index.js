95a0094896bc45d3c8281e04cb091c0bdd323fe1
Arqam Ali <arqamali@Arqams-MacBook-Pro.local>
2020-12-15 14:50:09 +0400

    db configurations

--- db/index.js
+++ db/index.js
@@ -3,10 +3,11 @@ const pg = require('pg')
 const {Pool} = pg
 
 const pool = new Pool({
-    user: "arqamali",
-    host: "localhost",
-    database: 'investmentdb',
-    port: 5432
+    user: "kmsvpqnghijxaf",
+    host: "ec2-35-169-184-61.compute-1.amazonaws.com",
+    database: 'd2te5s9tkt5nm9',
+    port: 5432,
+    password:'bec95e5246bf7cff092fb2d623f6aadddccce2c22539cb13bfbe19ea24b8e529'
 });
 
 module.exports = pool
