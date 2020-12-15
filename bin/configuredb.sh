a0ca0d74497e9b7fb0102b499381cfc0c5a45f31
Arqam Ali <arqamali@Arqams-MacBook-Pro.local>
2020-12-15 19:33:29 +0400

    db user name

--- bin/configuredb.sh
+++ bin/configuredb.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 echo 'configuring db..'
-
-psql -U kmsvpqnghijxaf d2te5s9tkt5nm9 < ./bin/investment.sql
-
+PGPASSWORD=bec95e5246bf7cff092fb2d623f6aadddccce2c22539cb13bfbe19ea24b8e529 psql --host=ec2-35-169-184-61.compute-1.amazonaws.com --user=kmsvpqnghijxaf  d2te5s9tkt5nm9 < ./bin/investment.sql
 echo 'db configured!'
