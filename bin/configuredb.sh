d75d7dd0af18971a7581967449ef40fe47f93796
Arqam Ali <arqamali@Arqams-MacBook-Pro.local>
2020-12-15 19:04:41 +0400

    db configured

--- bin/configuredb.sh
+++ bin/configuredb.sh
@@ -2,8 +2,6 @@
 
 echo 'configuring db..'
 
-createdb -U kmsvpqnghijxaf d2te5s9tkt5nm9
-
-psql -U kmsvpqnghijxaf d2te5s9tkt5nm9 < ../bin/investment.sql
+psql -U kmsvpqnghijxaf d2te5s9tkt5nm9 < ./bin/investment.sql
 
 echo 'db configured!'
