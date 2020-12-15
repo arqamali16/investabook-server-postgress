f64b7556f18e385c3bc90a25d79a1183624dd1a3
Arqam Ali <arqamali@Arqams-MacBook-Pro.local>
2020-12-15 19:41:47 +0400

    db user name

--- db/index.js
+++ db/index.js
@@ -10,4 +10,9 @@ const pool = new Pool({
     password:'bec95e5246bf7cff092fb2d623f6aadddccce2c22539cb13bfbe19ea24b8e529'
 });
 
+pool.query('SELECT * FROM users',(err,dbRes)=>{
+    if(err) console.log('error',err)
+    console.log(dbRes)
+})
+
 module.exports = pool
