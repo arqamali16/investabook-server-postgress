4939128f70b3229810b7ec429c5075939c83fccd
Arqam Ali <arqamali@Arqams-MacBook-Pro.local>
2020-12-17 12:45:07 +0400

    dockerised

--- Controller/payments.js
+++ Controller/payments.js
@@ -5,21 +5,21 @@ const app = express()
 app.use(bodyParser.urlencoded({extended:false}))
 
 
-const Payment = require('../Model/payments')
+// const Payment = require('../Model/payments')
 
 const getPayments = (req,res) =>{
-    const payments =  Payment.getPayments()
+    // const payments =  Payment.getPayments()
      res.send(payments)
 }
 
 const getPayment = (req,res) =>{
     const payment_id = req.params.payment_id
-    Payment.getPaymentsById(payment_id)
+    // Payment.getPaymentsById(payment_id)
 }
 
 const addPayment = (req,res) =>{
-    const payment = new Payment(req.body.date,req.body.amount,req.body.reciever,req.body.payer)
-    payment.add()
+    // const payment = new Payment(req.body.date,req.body.amount,req.body.reciever,req.body.payer)
+    // payment.add()
     res.send('added')
 }
 
--- db/index.js
+++ db/index.js
@@ -8,11 +8,9 @@ const pool = new Pool({
     database: 'd2te5s9tkt5nm9',
     port: 5432,
     password:'bec95e5246bf7cff092fb2d623f6aadddccce2c22539cb13bfbe19ea24b8e529',
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
-pool.query('SELECT * FROM users',(err,dbRes)=>{
-    if(err) console.log('error',err)
-    console.log(dbRes)
-})
 
 module.exports = pool
