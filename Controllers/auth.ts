3465712df569a4da9b97fc7b77cf4acde70f64ad
Arqam Ali <arqam.ali16@gmail.com>
2021-02-17 15:44:40 +0400

    login authorisation

--- Controllers/auth.ts
+++ Controllers/auth.ts
@@ -1,17 +1,26 @@
 import pool from '../db';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import _ from 'lodash';
 import { config } from '../config';
 
 const login = async (req: any, res: any) => {
 	const { name, email, password } = req.body;
-	// password hashing
-
-	const hashedPassword = await bcrypt.hash(password, 12);
-	var token = jwt.sign({ id: 'user id from db' }, config.secret, {
-		expiresIn: 86400, // expires in 24 hours
+	pool.query('SELECT * FROM owners WHERE email = $1', [email], async (err: any, dbRes: any) => {
+		if (err) res.status(404).send({ status: false, message: err });
+		if (_.isEmpty(dbRes.rows)) res.status(404).send({ status: false, message: 'No user found' });
+		else {
+			const isMatched = await bcrypt.compare(password, _.get(_.first(dbRes.rows), 'password'));
+			if (isMatched) {
+				var token = jwt.sign({ id: 'user id from db' }, config.secret, {
+					expiresIn: 60, // expires in 24 hours
+				});
+				res.status(200).send({ auth: true, token: token });
+			} else {
+				res.status(200).send({ status: false, message: 'password did not match' });
+			}
+		}
 	});
-	res.status(200).send({ auth: true, token: token });
 };
 
 export default { login };
